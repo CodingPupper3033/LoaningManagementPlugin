f235ed81b25c70a4cc3820c69f2be0f02dfefd0c
Joshua Miller <37462274+CodingPupper3033@users.noreply.github.com>
2023-11-10 14:35:49 -0500

    Fixed having duplicate files.
    Also, working on getting adding js to work

---
+++ static/test.js
@@ -0,0 +1,6 @@
+
+
+function loadTestTable(table) {
+    console.log('Loading test table');
+    table.bootstrapTable('test')
+}
--- templates/test.js
+++
@@ -1,7 +0,0 @@
-{% load i18n %}
-{% load inventree_extras %}
-{% load generic %}
-
-function loadTestTable(table, options) {
-    table.bootstrapTable('test')
-}
